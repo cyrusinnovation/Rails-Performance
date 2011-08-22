e630ed7babcfef05a279ad16f0ef440c56e752a2
Cyrus Innovation <cyrusinnovation@Bolivar-4.local>
2011-08-22 17:12:33 -0400

    [Paul, David] Added a jmeter test and used the cookie instead of the session to store the user id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:login], params[:password])
     if user
-      session[:user_id] = user.id
+      cookies[:user_id] = user.id
       redirect_to_target_or_default root_url#, :notice => "Logged in successfully."
     else
       flash.now[:alert] = "Invalid login or password."
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    cookies[:user_id] = nil
     redirect_to root_url, :notice => "You have been logged out."
   end
 end
--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,7 +1,7 @@
 class TweetController < ApplicationController
   def new
     if !session[:user_id].blank?
-      @user = User.find(session[:user_id])
+      @user = User.find(cookies[:user_id])
       @listed_tweets = @user.tweets
     else
       redirect_to login_path
@@ -9,7 +9,7 @@ class TweetController < ApplicationController
   end
 
   def create
-    user = User.find(session[:user_id])
+    user = User.find(cookies[:user_id])
     @tweet = Tweet.new(params[:tweet])
     @tweet.user = user
     @tweet.save!
